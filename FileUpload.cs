1e98e0770116956f37bd8b4f225d4fd6b74c4cc4
NeedHelper04 <needhelper04@gmail.com>
2023-10-07 20:22:45 +0530

    Add project files.

---
+++ FileUpload.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Azure.Storage.Blobs;
+
+namespace FileUploadFunction
+{
+    public static class FileUpload
+    {
+        [FunctionName("FileUpload")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string Connection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            string containerName = Environment.GetEnvironmentVariable("ContainerName");
+            Stream myBlob = new MemoryStream();
+            var file = req.Form.Files["File"];
+            myBlob = file.OpenReadStream();
+            var blobClient = new BlobContainerClient(Connection, containerName);
+            var blob = blobClient.GetBlobClient(file.FileName);
+            await blob.UploadAsync(myBlob);
+            return new OkObjectResult("file uploaded successfylly");
+        }
+    }
+}
